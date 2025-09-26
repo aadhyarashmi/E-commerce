dad07255962d0084a59b379c55e080d3a595ee41
Aadhya Rashmi <rashmiaadhya08@gmail.com>
2025-09-26 20:49:26 +0530

    Backend Setup with Express and Mongodb

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router= express.Router();
+const { getAllProduct } = require("../controllers/productController");
+router.route("/products").get(getAllProduct);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase= require("./config/database");
+//config
+dotenv.config({path: "backend/config/config.env"})
+//connect to database
+connectDatabase();
+app.listen(process.env.PORT,()=>{
+    console.log('Server is working at http://localhost:${process.env.PORT}')
+    
+})
